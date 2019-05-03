aabf7ab997f11023c98a99b64bfdc72f3144818c
Hironori <stdiff@outlook.de>
2019-05-04 00:11:25 +0200

    Metric files added in run scripts

--- run/01-loading.sh
+++ run/01-loading.sh
@@ -7,5 +7,6 @@ source <(run/configuration-parser.py --ini config.ini)
 dvc run -d ${script[loading]} \
         -o ${load[raw]} \
         -f run/01-loading.dvc \
+        -M ${load[metric]} \
         --overwrite-dvcfile \
         python ${script[loading]} --round ${general[round]}
--- run/02-processing.sh
+++ run/02-processing.sh
@@ -7,5 +7,6 @@ source <(run/configuration-parser.py --ini config.ini)
 dvc run -d ${load[raw]} -d ${script[processing]} \
         -o ${process[training]} -o ${process[test]} \
         -f run/02-processing.dvc \
+        -M ${process[metric]} \
         --overwrite-dvcfile \
         python ${script[processing]} --test_size 0.4 --random_state 42
